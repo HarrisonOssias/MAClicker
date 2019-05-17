7b3380639018fa70baf649c82a87696f82624082
Harrison Ossias <ossiash@mcmaster.ca>
2019-05-17 09:52:06 -0400

    u

--- MaClick/Instructor/Creat Peer Review/CreatePR.js
+++ MaClick/Instructor/Creat Peer Review/CreatePR.js
@@ -3,27 +3,21 @@ count = 0
 $(function () {
         $("#addG").on("click", function () {
             count++;
-            
             var nameForm = '<input id=in'+count+' type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Group Name"></input>';
             var spacer = '<br id=sp' + count + '>';
             var breaking = '<br id=break' + count + '>';
             var div1 = '<div>', div2 = '</div>';
             var size = '<input id=size'+count+' type="number" class="form-control " placeholder="Size">';
-            
-        
             var full = spacer + '<form class="form-inline">' + nameForm + size +  '</form>';
+            
             $('#addGroup').append(div1, full, div2);
-
             if (count == lim) {
                 alert("Limit of 30 groups met");
             }
-
-            alert(document.getElementById("size" + count).value);
-            /*for (i=0; i++; i<=$("#size" + count).val()){
+            for (i=0; i++; i<=$("#size" + count).val()){
                 var member = '<input id=mem'+ i+1 +' type="number" class="form" placeholder="Member"' + i+1 +'>';
                 $('#addGroup').append(div1, member, div2);
-            }*/
-
+            }
         });    
         $("#takeG").on("click", function () {
             $('#in' + count).remove();
@@ -33,6 +27,6 @@ $(function () {
             $('#size' + count).remove();
             count --;
         });
-        
+
     });
     

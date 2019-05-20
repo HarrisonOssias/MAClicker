2fa5c012dc0b4f80ecd1390ec8f6d70ca81c3f0b
Harrison Ossias <ossiash@mcmaster.ca>
2019-05-20 12:00:23 -0400

    updated

--- MaClick/Instructor/Creat Peer Review/CreatePR.js
+++ MaClick/Instructor/Creat Peer Review/CreatePR.js
@@ -3,22 +3,22 @@ count = 0
 $(function () {
         $("#addG").on("click", function () {
             count++;
-            var nameForm = '<input id=in'+count+' type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Group Name"></input>';
-            var spacer = '<br id=sp' + count + '>';
-            var breaking = '<br id=break' + count + '>';
-            var div1 = '<div>', div2 = '</div>';
-            var size = '<input id=size'+count+' type="number" class="form-control " placeholder="Size">';
+            var nameForm = '<input id=in'+count+' type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" placeholder="Group Name"></input>'
+            , spacer = '<br id=sp' + count + '>'
+            , breaking = '<br id=break' + count + '>'
+            , div1 = '<div>', div2 = '</div>'
+            , size = '<input id=size'+count+' type="number" class="form-control " placeholder="Size">';
+
             var full = spacer + '<form class="form-inline">' + nameForm + size +  '</form>';
             
             $('#addGroup').append(div1, full, div2);
             if (count == lim) {
                 alert("Limit of 30 groups met");
             }
-            for (i=0; i++; i<=$("#size" + count).val()){
-                var member = '<input id=mem'+ i+1 +' type="number" class="form" placeholder="Member"' + i+1 +'>';
-                $('#addGroup').append(div1, member, div2);
-            }
+            
+
         });    
+
         $("#takeG").on("click", function () {
             $('#in' + count).remove();
             $('#name' + count).remove();
